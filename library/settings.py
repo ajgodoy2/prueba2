4df02e2e19033146d08fe9b17d6da955dcb4d9c0
jreyesr <jreyesr@users.noreply.github.com>
2018-10-12 20:47:08 -0500

    Debug must be off in production!

--- library/settings.py
+++ library/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(e-9#45n)(s5)n_j2p=2og*bzi6gyu6fw0e-034ru!f*$gu__@'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",'(e-9#45n)(s5)n_j2p=2og*bzi6gyu6fw0e-034ru!f*$gu__@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
